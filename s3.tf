0ab76cc9772708e90f863a61b8a98ea201725df4
Jevgenijs Fedotovs <jfedot@gmail.com>
2020-01-07 15:46:13 +0200

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -42,4 +42,6 @@ resource "aws_s3_bucket" "nexus" {
 resource "aws_s3_account_public_access_block" "block" {
   block_public_acls   = true
   block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
